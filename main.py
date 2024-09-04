bfc47dfb4d1ba85e5373538a0f9c4cd6d11e44d4
retamprog <120020649+retamprog@users.noreply.github.com>
2024-09-04 13:18:19 +0530

    third commit

--- main.py
+++ main.py
@@ -98,5 +98,71 @@ elif appmode=='Contact us':
        **Phone Number :** 7439729596                                           
 
     ''') 
-    
-elif appmode==''    
+
+elif appmode=='About':
+    st.markdown(''' 
+    ## About Dataset
+                
+    This dataset is recreated using offline augmentation from the original dataset. The original dataset can be found on this github repo. 
+    This dataset consists of about 87K rgb images of healthy and diseased crop leaves which is categorized into 38 different classes. 
+    The total dataset is divided into 80/20 ratio of training and validation set preserving the directory structure. 
+    A new directory containing 33 test images is created later for prediction purpose.
+
+    ### Content:
+    1) **Train** : 70295 files belonging to 38 classes.
+    2) **valid** : 17572 files belonging to 38 classes.
+    3) **Test**  : 33 test images for prediction purposes.            
+                
+                                                    
+
+    ''')    
+elif appmode=='Disease Detection':
+    st.header('Crop Disease Detection')
+    test_image = st.file_uploader("upload your image here")
+    if st.button('show image'):
+        st.image(test_image,use_column_width=True)    
+    if st.button('predict'):
+      with st.spinner('Please wait..'):
+         st.balloons()
+         # here we will call our model for prediction
+         st.write('Dl Prediction')
+         index = modelpredictions(test_image)
+         class_name=['Apple___Apple_scab',
+            'Apple___Black_rot',
+            'Apple___Cedar_apple_rust',
+            'Apple___healthy',
+            'Blueberry___healthy',
+            'Cherry_(including_sour)___Powdery_mildew',
+            'Cherry_(including_sour)___healthy',
+            'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot',
+            'Corn_(maize)___Common_rust_',
+            'Corn_(maize)___Northern_Leaf_Blight',
+            'Corn_(maize)___healthy',
+            'Grape___Black_rot',
+            'Grape___Esca_(Black_Measles)',
+            'Grape___Leaf_blight_(Isariopsis_Leaf_Spot)',
+            'Grape___healthy',
+            'Orange___Haunglongbing_(Citrus_greening)',
+            'Peach___Bacterial_spot',
+            'Peach___healthy',
+            'Pepper,_bell___Bacterial_spot',
+            'Pepper,_bell___healthy',
+            'Potato___Early_blight',
+            'Potato___Late_blight',
+            'Potato___healthy',
+            'Raspberry___healthy',
+            'Soybean___healthy',
+            'Squash___Powdery_mildew',
+            'Strawberry___Leaf_scorch',
+            'Strawberry___healthy',
+            'Tomato___Bacterial_spot',
+            'Tomato___Early_blight',
+            'Tomato___Late_blight',
+            'Tomato___Leaf_Mold',
+            'Tomato___Septoria_leaf_spot',
+            'Tomato___Spider_mites Two-spotted_spider_mite',
+            'Tomato___Target_Spot',
+            'Tomato___Tomato_Yellow_Leaf_Curl_Virus',
+            'Tomato___Tomato_mosaic_virus',
+            'Tomato___healthy']
+         st.success(f"The Dl model is predicting {class_name[index]}") 
